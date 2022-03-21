66c7e9256328f34e39898cac3e951f316f5a04b9
Devin McQueeney <devin.mcqueeney@delawarelife.com>
2022-03-21 13:58:17 -0400

    remove styled

--- components/Builder/EditPanel/EditPanel.tsx
+++ components/Builder/EditPanel/EditPanel.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useEditor } from "@craftjs/core";
 import { Layers } from "@craftjs/layers";
-import styled from "styled-components";
 import { useRouter } from "next/router";
 import lz from "lzutf8";
 
@@ -13,13 +12,10 @@ import { Button } from "../../Elements/Button/Button/Button";
 import { Icon } from "../../Elements/Media/Icon/Icon";
 import { Text } from "../../Elements/Media/Text/Text";
 import { Popup } from "../../Elements/Container/Popup/Popup";
+import { Nav } from "../../Elements/Container/Nav/Nav";
+import { Div } from "../../Elements/Container/Div/Div";
 
 const barWidth = 350;
-export const SidebarDiv = styled.div<{ enabled: boolean }>`
-  width: ${barWidth}px;
-  opacity: ${(props) => (props.enabled ? 1 : 0)};
-  margin-right: ${(props) => (props.enabled ? 0 : -barWidth)}px;
-`;
 
 export const EditPanel = ({ store }): JSX.Element => {
   const [path, setPath] = useState(null);
@@ -104,169 +100,175 @@ export const EditPanel = ({ store }): JSX.Element => {
   };
 
   return (
-    <SidebarDiv
-      enabled={enabled}
+    <Div
+      style={{
+        width: barWidth + "px",
+        opacity: enabled ? 1 : 0,
+        marginRight: enabled ? 0 : -barWidth + "px",
+      }}
       className="sidebar transition w-2 flex flex-col h-full overflow-y-auto bg-slate-700"
     >
-      <ul className="flex mr-3 bg-slate-700 text-white/75 w-full p-1 text-sm">
-        <li className="flex w-full">
-          <Button
-            type="button"
-            tooltip="Close"
-            onClick={cancelEdit}
-            placement="bottom"
-            className="px-2"
-          >
-            <Icon className="fa-solid fa-times" />
-          </Button>
-        </li>
-
-        <li className="flex">
-          <Popup
-            menu={
-              <div className="text-xs bg-slate-800 text-white w-full">
-                <Button
-                  onClick={(e) => changeScreen(e, "mobile")}
-                  type="button"
-                  className="block w-full py-1 hover:bg-slate-900 select-none cursor-pointer"
-                >
-                  <Icon className="fa-solid fa-mobile-screen mr-1" />
-                  <Text type="span" text="Mobile" disabled={true} />
-                </Button>
-                <Button
-                  onClick={(e) => changeScreen(e, "tablet")}
-                  type="button"
-                  className="block w-full py-1 hover:bg-slate-900 select-none cursor-pointer"
-                >
-                  <Icon className="fa-solid fa-tablet-screen mr-1" />
-                  <Text type="span" text="Tablet" disabled={true} />
-                </Button>
-                <Button
-                  onClick={(e) => changeScreen(e, "screen")}
-                  type="button"
-                  className="block w-full py-1 hover:bg-slate-900 select-none cursor-pointer"
-                >
-                  <Icon className="fa-solid fa-display mr-1" />
-                  <Text type="span" text="Fullscreen" disabled={true} />
-                </Button>
-              </div>
-            }
-            position="bottom center"
-          >
+      <Nav className="bg-slate-900 text-white/75 w-full text-sm">
+        <ul className="flex mx-1 w-full">
+          <li className="flex w-full">
             <Button
               type="button"
-              tooltip={screen}
+              tooltip="Close"
+              onClick={cancelEdit}
+              placement="bottom"
+              className="px-2 py-1 hover:bg-slate-800"
+            >
+              <Icon className="fa-solid fa-times" />
+            </Button>
+          </li>
+
+          <li className="flex">
+            <Popup
+              menu={
+                <div className="text-xs bg-slate-800 text-white w-full">
+                  <Button
+                    onClick={(e) => changeScreen(e, "mobile")}
+                    type="button"
+                    className="block w-full py-1 hover:bg-slate-900 select-none cursor-pointer"
+                  >
+                    <Icon className="fa-solid fa-mobile-screen mr-1" />
+                    <Text type="span" text="Mobile" disabled={true} />
+                  </Button>
+                  <Button
+                    onClick={(e) => changeScreen(e, "tablet")}
+                    type="button"
+                    className="block w-full py-1 hover:bg-slate-900 select-none cursor-pointer"
+                  >
+                    <Icon className="fa-solid fa-tablet-screen mr-1" />
+                    <Text type="span" text="Tablet" disabled={true} />
+                  </Button>
+                  <Button
+                    onClick={(e) => changeScreen(e, "screen")}
+                    type="button"
+                    className="block w-full py-1 hover:bg-slate-900 select-none cursor-pointer"
+                  >
+                    <Icon className="fa-solid fa-display mr-1" />
+                    <Text type="span" text="Fullscreen" disabled={true} />
+                  </Button>
+                </div>
+              }
+              position="bottom center"
+            >
+              <Button
+                type="button"
+                tooltip={screen}
+                placement="bottom"
+                className="px-2"
+              >
+                <Icon
+                  className={
+                    "fa-solid " +
+                    (screen === "mobile"
+                      ? "fa-mobile-screen"
+                      : screen === "tablet"
+                      ? "fa-tablet-screen"
+                      : "fa-display")
+                  }
+                />
+              </Button>
+            </Popup>
+          </li>
+
+          <li className="flex">
+            <Button
+              type="button"
+              tooltip="Outlines"
+              placement="bottom"
+              onClick={(e) => toggleOutlines(e)}
+              disabled={!outlinesVisible}
+              className="px-2"
+            >
+              <Icon
+                className={
+                  "fa-solid fa-square-dashed" +
+                  (outlinesVisible ? "" : " opacity-50")
+                }
+              />
+            </Button>
+          </li>
+
+          <li className="flex">
+            <Button
+              type="button"
+              tooltip="Ruler"
+              placement="bottom"
+              onClick={toggleRuler}
+              disabled={!rulerVisible}
+              className="px-2"
+            >
+              <Icon
+                className={
+                  "fa-solid fa-ruler" + (rulerVisible ? "" : " opacity-50")
+                }
+              />
+            </Button>
+          </li>
+
+          <li className="flex">
+            <Button
+              type="button"
+              tooltip="Remove All"
+              onClick={clear}
+              placement="bottom"
+              className="px-2"
+            >
+              <Icon className="fa-solid fa-trash" />
+            </Button>
+          </li>
+
+          <li className="flex">
+            <Button
+              type="button"
+              tooltip="Undo"
+              placement="bottom"
+              onClick={() => actions.history.undo()}
+              disabled={!canUndo}
+              className="px-2"
+            >
+              <Icon
+                className={"fa-solid fa-undo" + (canUndo ? "" : " opacity-50")}
+              />
+            </Button>
+          </li>
+
+          <li className="flex">
+            <Button
+              type="button"
+              tooltip="Redo"
+              placement="bottom"
+              disabled={!canRedo}
+              onClick={() => actions.history.redo()}
+              className="px-2"
+            >
+              <Icon
+                className={"fa-solid fa-redo" + (canRedo ? "" : " opacity-50")}
+              />
+            </Button>
+          </li>
+
+          <li className="flex gap-2">
+            <Button
+              type="button"
+              onClick={saveChanges}
+              disabled={!canUndo}
+              tooltip={enabled ? "Save" : "Edit"}
               placement="bottom"
               className="px-2"
             >
               <Icon
                 className={
-                  "fa-solid " +
-                  (screen === "mobile"
-                    ? "fa-mobile-screen"
-                    : screen === "tablet"
-                    ? "fa-tablet-screen"
-                    : "fa-display")
+                  "fa-solid fa-floppy-disk" + (canUndo ? "" : " opacity-50")
                 }
               />
             </Button>
-          </Popup>
-        </li>
-
-        <li className="flex">
-          <Button
-            type="button"
-            tooltip="Outlines"
-            placement="bottom"
-            onClick={(e) => toggleOutlines(e)}
-            disabled={!outlinesVisible}
-            className="px-2"
-          >
-            <Icon
-              className={
-                "fa-solid fa-square-dashed" +
-                (outlinesVisible ? "" : " opacity-50")
-              }
-            />
-          </Button>
-        </li>
-
-        <li className="flex">
-          <Button
-            type="button"
-            tooltip="Ruler"
-            placement="bottom"
-            onClick={toggleRuler}
-            disabled={!rulerVisible}
-            className="px-2"
-          >
-            <Icon
-              className={
-                "fa-solid fa-ruler" + (rulerVisible ? "" : " opacity-50")
-              }
-            />
-          </Button>
-        </li>
-
-        <li className="flex">
-          <Button
-            type="button"
-            tooltip="Remove All"
-            onClick={clear}
-            placement="bottom"
-            className="px-2"
-          >
-            <Icon className="fa-solid fa-trash" />
-          </Button>
-        </li>
-
-        <li className="flex">
-          <Button
-            type="button"
-            tooltip="Undo"
-            placement="bottom"
-            onClick={() => actions.history.undo()}
-            disabled={!canUndo}
-            className="px-2"
-          >
-            <Icon
-              className={"fa-solid fa-undo" + (canUndo ? "" : " opacity-50")}
-            />
-          </Button>
-        </li>
-
-        <li className="flex">
-          <Button
-            type="button"
-            tooltip="Redo"
-            placement="bottom"
-            disabled={!canRedo}
-            onClick={() => actions.history.redo()}
-            className="px-2"
-          >
-            <Icon
-              className={"fa-solid fa-redo" + (canRedo ? "" : " opacity-50")}
-            />
-          </Button>
-        </li>
-
-        <li className="flex gap-2">
-          <Button
-            type="button"
-            onClick={saveChanges}
-            disabled={!canUndo}
-            tooltip={enabled ? "Save" : "Edit"}
-            placement="bottom"
-            className="px-2"
-          >
-            <Icon
-              className={
-                "fa-solid fa-floppy-disk" + (canUndo ? "" : " opacity-50")
-              }
-            />
-          </Button>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </Nav>
 
       <ComponentsPanel />
 
@@ -275,6 +277,6 @@ export const EditPanel = ({ store }): JSX.Element => {
       <DataPanel store={store} path={path} />
 
       <Layers expandRootOnLoad={true} />
-    </SidebarDiv>
+    </Div>
   );
 };
